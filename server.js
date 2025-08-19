502f4d96460aeee088578bec7b141a9a49325fdf
unyime101 <unyime@gmail.com>
2025-08-19 15:43:09 +0100

    Ejs templates created.

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ const express = require('express');
 // express app
 const app = express();
 
+// registering view eng
+app.set("view engine","ejs");
+app.set("views","pages");
 // listen for requests
 app.listen(3000);
 
-app.get('/', (req, res) => { //directs to index page. Makes sure path is always the say from current wd
-  res.sendFile('./pages/index.html', { root: __dirname });
+app.get("/", (req, res) => { //directs to index page. Makes sure path is always the say from current wd
+    res.render("index",{title: "home"});
 });
 
-app.get('/aboutme', (req, res) => {
-  res.sendFile('./pages/aboutme.html', { root: __dirname });
+app.get("/aboutme", (req, res) => {
+  res.render("aboutme",{title:"About Me"});
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
-});
+app.get("/create", (req, res)=>{
+    res.render("create", {title:"Create New post"});
+})
 
 // 404 page
-app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
-});
+app.use((req, res) => { //assumes error code. checks if the status is 404. shows the rror page
+  res.status(404).render("404", {title:"404"});})
+
+//express is so cool
