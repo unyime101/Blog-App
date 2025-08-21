246293213c2c31cf290c4b4b16391b60932f79c9
unyime101 <unyime@gmail.com>
2025-08-21 23:15:02 +0100

    DB conn done.

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+require("dotenv").config(); // for my secret files shh
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 // express app
 const app = express();
+//connection string for mongodb stored in .env
+const c_string = process.env.MONGO_URI;
+// console.log("The string: ",c_string); 
 
+//connecting to mongo db
+mongoose.connect(c_string)// basically a try catch 
+    .then((result)=> app.listen(3000))
+    .catch((err)=> console.log(err));
 // registering view eng
 app.set("view engine","ejs");
 app.set("views","pages");
@@ -12,7 +21,7 @@ app.set("views","pages");
 app.use(morgan("dev")); //logs working
 app.use(express.static("public"));
 // listen for requests
-app.listen(3000);
+
 
 app.get("/", (req, res) => { //directs to index page. Makes sure path is always the say from current wd
     const blogs = [{title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
